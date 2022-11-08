f4a0648863885b9e0f5cfa5d5fa7d096a449f7b5
kishore-klassy <kishorekumararcot@gmail.com>
2022-11-08 23:44:19 +0530

    version 1.1

--- MouseInfo.java
+++ MouseInfo.java
@@ -8,24 +8,8 @@ public MouseInfo(String brand,String mouseType)
 {
 		this.brand = brand ;
 		this.mouseType = mouseType ;
-		
-	
-	
-	
 }
 
 
-/* public void mouseMethod() {
-
-	System.out.println("I am Mouse.I can Scroll,right click,left click");
-
-	
-	
-	
-	
-} */
-
 
 }
-/* 	System.out.println("--MOUSE--");
-	System.out.println("BRAND : "+ */
